3d16d125b6617712b18577f0d2ac1eaf8025d501
Boris <borisam07@gmail.com>
2025-03-25 15:11:22 +0100

    first commit

---
+++ RandomNummer.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Random Nummer")
+    root.geometry("400x500")
+    root.mainloop()
+
+
+import random
+
+print(random.randint(1, 100)) # Random nummer zwischn 1 und 100
+
+
